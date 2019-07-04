6efc1710c1bad488dbc4d52f1413a91f7ea3c439
wangjie <1358726405@qq.com>
2019-07-04 10:15:09 +0800

    加入题目说明

--- src/test/java/cn/wangjie/learn/leetcode/LongestPalindrome.java
+++ src/test/java/cn/wangjie/learn/leetcode/LongestPalindrome.java
@@ -9,6 +9,18 @@ import java.util.List;
 /**
  * @program: learn
  * @description: 最长回文子串
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
  * @author: WangJie
  * @create: 2019-06-19 10:40
  **/
